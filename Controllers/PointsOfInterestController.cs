8f9b5edbd582162881c64ec8fe696a5d67269894
Jason K. Babie <jasonb@property24.com>
2023-07-02 20:42:53 +0200

    Add implementation for posting resources

--- Controllers/PointsOfInterestController.cs
+++ Controllers/PointsOfInterestController.cs
@@ -19,7 +19,7 @@ namespace CityInfo.API.Controllers
             return Ok(city.PointsOfInterest);
         }
 
-        [HttpGet("{pointOfInterestId}")]
+        [HttpGet("{pointOfInterestId}", Name = "GetPointOfInterest")]
         public ActionResult<PointOfInterestDTO> GetPointOfInterest(int cityId, int pointOfInterestId)
         {
             var city = CityDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
@@ -27,12 +27,37 @@ namespace CityInfo.API.Controllers
             {
                 return NotFound();
             }
-            var pointOfInterest = city.PointsOfInterest.FirstOrDefault(p =>  p.Id == pointOfInterestId);
+            var pointOfInterest = city.PointsOfInterest.FirstOrDefault(p => p.Id == pointOfInterestId);
             if (pointOfInterest == null)
             {
                 return NotFound();
             }
             return Ok(pointOfInterest);
+        }
+
+        [HttpPost]
+        public ActionResult<PointOfInterestDTO> CreatePointOfInterest(int cityId, PointOfInterestForCreationDTO pointOfInterest)
+        {
+            var city = CityDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
+            if (city == null)
+            {
+                return NotFound();
+            }
+
+            var maxID = CityDataStore.Current.Cities.SelectMany(c => c.PointsOfInterest).Max(p => p.Id);
+            var newPointOfInterest = new PointOfInterestDTO()
+            {
+                Id = ++maxID,
+                Description = pointOfInterest.Description,
+                Name = pointOfInterest.Name,
+            };
+            city.PointsOfInterest.Add(newPointOfInterest);
+            return CreatedAtRoute("GetPointOfInterest",
+                new
+                {
+                    cityId = cityId,
+                    pointOfInterestId = newPointOfInterest.Id
+                }, newPointOfInterest);
         }
     }
 }
